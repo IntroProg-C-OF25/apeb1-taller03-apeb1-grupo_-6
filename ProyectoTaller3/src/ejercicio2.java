aac09387777f2bd976b42aad1cdb55a659e9f094
Dagonzález2006 <dudef.2222.11@gmail.com>
2024-10-24 12:57:27 -0500

    Solucion del problema 2

--- ProyectoTaller3/src/ejercicio2.java
+++ ProyectoTaller3/src/ejercicio2.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author L E N O V O
- */
 public class ejercicio2 {
-    
+    public static void main(String[] args) {
+        double respuesta;   
+        respuesta = (10) + Math.pow(10, 2); 
+        System.out.println("respuesta = " + respuesta);
+    }
 }
